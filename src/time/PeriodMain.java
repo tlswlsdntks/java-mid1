a29bcb6f256d63561fb8f3c0f5605d2b89f141e3
tlswlsdntks <tlswlsdntks@naver.com>
2024-08-13 18:17:59 +0900

    java-mid1

--- src/time/PeriodMain.java
+++ src/time/PeriodMain.java
@@ -19,7 +19,7 @@ public class PeriodMain {
         LocalDate startDate = LocalDate.of(2023, 1, 1);
         LocalDate endDate = LocalDate.of(2023, 4, 2);
         Period between = Period.between(startDate, endDate);
-        System.out.println("between = " + between);
+        System.out.println("between = " + between); //P3M1D
         System.out.println("기간 = " + between.getMonths() + "개월 " + between.getDays() + "일");
     }
 }
