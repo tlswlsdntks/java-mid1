601768a0ee44370257a52ce4cabdad1caa0ddcc2
tlswlsdntks <naver.com>
2024-09-02 21:30:26 +0900

    java-mid1

--- src/nested/local/LocalOuterV4.java
+++ src/nested/local/LocalOuterV4.java
@@ -25,8 +25,7 @@ public class LocalOuterV4 {
         localVar = 10;
         paramVar = 10;
         /**
-         * 스택 영역에 존재하는 지역 변수의 값과 인스턴스에 캡처한 변수의 값이 서로 달라지는 문제가 발생한다.
-         * 이것을 동기화 문제라 한다.
+         * 스택 영역에 존재하는 지역 변수의 값과 인스턴스에 캡처한 변수의 값이 서로 달라지는 문제가 발생한다. 이것을 동기화 문제라 한다.
          * 자바는 캡처한 지역 변수의 값을 변하지 못하게 막아서 동기화 문제를 근본적으로 차단한다.
          * //System.out.println("localVar = " + localVar); //컴파일 에러
          * //System.out.println("paramVar = " + paramVar); //컴파일 에러
