8b7a5d0a990c1fccbc0eaef636db858017a7e3ed
tlswlsdntks <naver.com>
2024-09-05 23:25:37 +0900

    java-mid1

---
+++ src/exception/basic/checked/MyCheckedException.java
@@ -0,0 +1,10 @@
+package exception.basic.checked;
+
+/**
+ * Exception을 상속받은 예외는 체크 예외가 된다.
+ */
+public class MyCheckedException extends Exception {
+    public MyCheckedException(String message) {
+        super(message); //super(message)로 전달된 메세지는 Throwable에 있는 detailMessage에 보관된다.
+    }
+}
