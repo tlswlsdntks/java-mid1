520ae64c5f5f090c61f9684a53edf2522e044d85
tlswlsdntks <naver.com>
2024-10-13 22:33:07 +0900

    java-mid1

--- src/lang/string/method/StringSearchMain.java
+++ src/lang/string/method/StringSearchMain.java
@@ -8,8 +8,8 @@ public class StringSearchMain {
          * indexOf(String ch) / indexOf(String ch, int fromIndex) : 문자열이 처음 등장하는 위치를 반환한다.
          * lastIndexOf(String ch) : 문자열이 마지막으로 등장하는 위치를 반환한다.
          */
-        System.out.println("str.contains(\"java\") = " + str.contains("Java"));
-        System.out.println("str.indexOf(\"java\") = " + str.indexOf("Java"));
+        System.out.println("str.contains(\"Java\") = " + str.contains("Java"));
+        System.out.println("str.indexOf(\"Java\") = " + str.indexOf("Java"));
         System.out.println("str.indexOf(\"Java\",10) = " + str.indexOf("Java", 10));
         System.out.println("str.lastIndexOf(\"Java\") = " + str.lastIndexOf("Java"));
     }
