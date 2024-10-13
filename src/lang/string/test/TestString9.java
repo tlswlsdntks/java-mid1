c824bddd6b916c7462c717a9a1e51024f570e48e
tlswlsdntks <naver.com>
2024-10-13 22:52:56 +0900

    java-mid1

--- src/lang/string/test/TestString9.java
+++ src/lang/string/test/TestString9.java
@@ -2,7 +2,7 @@ package lang.string.test;
 
 public class TestString9 {
     public static void main(String[] args) {
-        String email = "helllo@example.com";
+        String email = "hello@example.com";
         String[] split = email.split("@");
         System.out.println("ID = " + split[0]);
         System.out.println("Domain = " + split[1]);
