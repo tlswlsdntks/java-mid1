781310977cd85271f66a948d6fef54d35618f0d0
tlswlsdntks <tlswlsdntks@naver.com>
2024-11-14 13:16:30 +0900

    java-mid1

--- src/nested/nested/NestedOuter.java
+++ src/nested/nested/NestedOuter.java
@@ -1,21 +1,21 @@
 package nested.nested;
 
 public class NestedOuter {
-    private static int outClassValue = 3;
     private int outInstanceValue = 2;
+    private static int outClassValue = 3;
 
     static class Netsed {
         private int nestedInsatnceValue = 1;
 
         public void print() {
             //자신의 멤버에 접근
-            System.out.println(nestedInsatnceValue);
+            System.out.println(nestedInsatnceValue); //1
 
             //바깥 클래스의 인스턴스 멤버에 접근은 불가능.
             //System.out.println(outInstanceValue);
 
             //바깥 클래스의 클래스 멤버에 접근 가능, private도 접근 가능
-            System.out.println(outClassValue);
+            System.out.println(outClassValue); //3
         }
     }
 }
