58c5eebfeed6a8a9adaf69e03c4c503521fc71e7
tlswlsdntks <tlswlsdntks@naver.com>
2024-12-18 17:24:31 +0900

    java-mid1

--- src/enumeration/ref1/DiscountService.java
+++ src/enumeration/ref1/DiscountService.java
@@ -2,9 +2,9 @@ package enumeration.ref1;
 
 public class DiscountService {
     public int discount(ClassGrade classGrade, int price) {
-        /**
-         * CTRL + ALT + N: INLINE VARIABLE
-         */
+        //Ctrl + Alt + N: Inline Variable
+        int i = 1;
+        System.out.println(i);
         return price * classGrade.getDiscountPercent() / 100;
     }
 }
