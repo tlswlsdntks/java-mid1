2e34f0ba9080cd974be08324e2d00d86862c1d5e
tlswlsdntks <tlswlsdntks@naver.com>
2024-12-23 17:12:25 +0900

    java-mid1

--- src/lang/string/method/StringSplitJoinMain.java
+++ src/lang/string/method/StringSplitJoinMain.java
@@ -2,7 +2,7 @@ package lang.string.method;
 
 public class StringSplitJoinMain {
     public static void main(String[] args) {
-        String str = "Apple,Banna,Orange";
+        String str = "Apple,Banna,Orange";  
         /**
          * split(String regex): 문자열을 정규 표현식을 기준으로 분할한다.
          * join(CharSequence delimiter, CharSequence... elements): 주어진 구분자로 여러 문자열을 결합한다.
--- src/lang/string/method/StringUtilsMain2.java
+++ src/lang/string/method/StringUtilsMain2.java
@@ -8,6 +8,7 @@ public class StringUtilsMain2 {
         /**
          * format(String format, Object... args): 형식 문자열과 인자를 사용하여 새로운 문자열을 생성한다.
          * matches(String regex): 문자열이 주어진 정규 표현식과 일치하는지 확인한다.
+         * System.out.printf(String format, Object obj): 지시자를 통해 변수의 값을 여러 가지 형식으로 변환하여 출력할 수 있다.
          */
         System.out.println("num = " + num);
         System.out.println("bool = " + bool);
