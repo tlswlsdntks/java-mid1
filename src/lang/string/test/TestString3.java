4b850c8842c4237ed40d670d46704480eabdefb6
tlswlsdntks <tlswlsdntks@naver.com>
2024-12-23 17:23:43 +0900

    java-mid1

--- src/lang/string/test/TestString3.java
+++ src/lang/string/test/TestString3.java
@@ -3,6 +3,7 @@ package lang.string.test;
 public class TestString3 {
     public static void main(String[] args) {
         String str = "hello.txt";
-        System.out.println("index = " + str.indexOf(".txt"));
+        int index = str.indexOf(".txt");
+        System.out.println("index = " + index);
     }
 }
